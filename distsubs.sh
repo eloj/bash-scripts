57d421b2c6eabf52b0e4388a0824450469040202
Eddy L O Jansson <eddy@klopper.net>
2023-01-11 12:43:38 +0100

    Add 'distsubs.sh'

---
+++ distsubs.sh
@@ -0,0 +1,23 @@
+#
+# Distribute Subs based on 'standard' scene format, where
+# there's a separate Subs directory in which there's one directory
+# per video file, named the same as the video file sans the extension,
+# which contains the subtitles.
+#
+#!/bin/bash
+for f in ./*.mp4; do
+	base=${f%%.mp4}
+	subpath=Subs/${base}/
+	if [ ! -d ${subpath} ]; then
+		echo "ERROR: Subtitle path '${subpath}' not found."
+		exit 1
+	fi
+
+	if [ -e ${subpath}/3_English.srt ]; then
+		cp ${subpath}/3_English.srt ${base}.srt
+	elif [ -e ${subpath}/2_English.srt ]; then
+		cp ${subpath}/2_English.srt ${base}.srt
+	else
+		echo "WARNING: No subtitles found in ${subpath}!"
+	fi
+done
