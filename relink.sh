c4b25ae1c15b4e21eb101e618d3a0027e449a279
Eddy Jansson <eddy@klopper.net>
2024-06-20 13:23:44 +0200

    Handle paths with spaces.

--- relink.sh
+++ relink.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+#
 # Change an existing link (arg1) to a new target (arg2)
-relink(){
+#
+relink() {
 	if [ ! -L "$1" ]; then echo "First argument must be an existing symbolic link" && return 1; fi
 	if [ ! -e "$2" ]; then echo "Target '$2' doesn't exist." && return 1; fi
-	ln -sfn `readlink -f $2` $1
+	# Canonicalize target link (alt. use realpath) -- makes symlink movable
+	TARGET=$(readlink -f "$2")
+	ln -sfn "$TARGET" "$1"
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
