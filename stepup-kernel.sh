a22b795adffdd4ffa760334eda7636fe28bd7b47
Eddy L O Jansson <eddy@klopper.net>
2024-11-22 18:50:14 +0100

    Add support for upgrading from .0

--- stepup-kernel.sh
+++ stepup-kernel.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 #
 # Upgrade linux-x.y.z to linux-x.y.z+1
+# by el0j -- https://github.com/eloj/bash-scripts/blob/master/stepup-kernel.sh
 #
 # Uses running kernel as base version. Override by passing version triplet as argument.
 #
-K_BASE_URL=https://cdn.kernel.org/pub/linux/kernel/vR_MAJOR.x/incr/
-K_BASE_NAME=patch-R_VER-D_LEVEL.xz
+BASE_URL=https://cdn.kernel.org/pub/linux/kernel
 R_VER=${1:-$(uname -r)}
 R_MAJOR=${R_VER%%.*}
-D_LEVEL=$((${R_VER##*.} + 1))
+R_LEVEL=${R_VER##*.}
+D_LEVEL=$((${R_LEVEL} + 1))
 NEXT_VER=${R_VER%.*}.$D_LEVEL
 
 [ ! -z $R_VER ] || { echo "Couldn't get running current kernel version" && exit 1; }
 
 echo "Upgrading from kernel $R_VER to $NEXT_VER"
 
-# Substitute R_VER and D_LEVEL into K_BASE
-PATCH_FILE=${K_BASE_NAME/R_VER/$R_VER}
-PATCH_FILE=${PATCH_FILE/D_LEVEL/$D_LEVEL}
-PATCH_URL=${K_BASE_URL/R_MAJOR/$R_MAJOR}${PATCH_FILE}
+if [ "$R_LEVEL" -eq 0 ]; then
+	# Full patch
+	PATCH_FILE=patch-${NEXT_VER}.xz
+	PATCH_URL=${BASE_URL}/v${R_MAJOR}.x/${PATCH_FILE}
+	# Rewrite R_VER without .0
+	R_VER=${R_VER%%.0}
+else
+	# Incremential patch
+	PATCH_FILE=patch-${R_VER}-${D_LEVEL}.xz
+	PATCH_URL=${BASE_URL}/v${R_MAJOR}.x/incr/${PATCH_FILE}
+fi
 
 # Download file, if missing.
 if [ ! -f $PATCH_FILE ]; then
@@ -53,4 +61,4 @@ mv linux-$R_VER linux-$NEXT_VER
 echo "Removing patch file"
 rm ${PATCH_FILE}
 
-echo "Suggest: sudo ./kinstall $NEXT_VER"
+echo "Suggest: sudo ./kinstall.sh $NEXT_VER"
